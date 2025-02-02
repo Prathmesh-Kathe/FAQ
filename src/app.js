4e0193c28dfcdba4d3a804b494a2353b0afdb57d
Prathmesh Kathe <prathmesh292000@gmail.com>
2025-02-02 18:10:14 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const path = require("path");
+const connectDB = require("./config/db");
+const faqRoutes = require("./routes/faqRoutes");
+const faqRoutes2 = require('./routes/adminRoute')
+
+const app = express();
+// Connect to the database
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Set up static files
+// const rootDir = path.resolve(__dirname); // Use __dirname for stability
+// app.use(express.static(path.join(rootDir, "public")));
+
+// Set EJS as the view engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views")); // Ensure correct path
+
+// API Routes
+app.use("/api", faqRoutes);
+app.use("/" , faqRoutes2)
+
+
+module.exports = app;
---
+++ src/routes/faqRoutes.js
@@ -0,0 +1,20 @@
+// const express = require("express");
+// const { createFAQ, getFAQs } = require("../controllers/faqController");
+
+// const router = express.Router();
+// router.post("/faqs", createFAQ);
+// router.get("/faqs", getFAQs);
+
+// module.exports = router;
+
+const express = require("express");
+const { createFAQ ,  getFAQs} = require("../controllers/faqController.js"); 
+
+const router = express.Router();
+
+router.post("/faqs", createFAQ);
+
+router.get("/faqs", getFAQs);
+
+
+module.exports = router;
